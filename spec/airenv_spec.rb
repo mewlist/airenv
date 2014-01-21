f073386c4141de4a459eeb4b769f147e525d24c7
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2014-01-21 15:21:16 +0900

    generate rspec template
    bundle gem airenv -t

---
+++ spec/airenv_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Airenv do
+  it 'should have a version number' do
+    Airenv::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should be_true
+  end
+end
