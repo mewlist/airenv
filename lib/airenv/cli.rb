1805a2023f729bc54bc1d29f1f1e0381ee8275ee
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2014-01-21 18:02:43 +0900

    Fixes version commnad description

--- lib/airenv/cli.rb
+++ lib/airenv/cli.rb
@@ -2,7 +2,7 @@ require 'thor'
 
 class Airenv::CLI < Thor
 
-  desc 'Prints this version', 'Prints Airenv version'
+  desc 'version', 'Prints Airenv version'
   def version
     puts "Airenv #{Airenv::VERSION}"
   end
