cab4b8b64a03569929fa7a44d009723500c0ba95
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2014-01-21 18:03:30 +0900

    Adds automatic task loader

--- lib/airenv/cli.rb
+++ lib/airenv/cli.rb
@@ -1,10 +1,19 @@
 require 'thor'
+require 'active_support/inflector'
 
 class Airenv::CLI < Thor
 
   desc 'version', 'Prints Airenv version'
   def version
     puts "Airenv #{Airenv::VERSION}"
+  end
+
+  def self.load_thorfiles(dir)
+    thor_files = Dir.glob('**/*.thor').delete_if { |x| not File.file?(x) }
+    thor_files.each do |f|
+      load f
+      include f.to_s.gsub('lib/', '').gsub('.thor', '').camelize.constantize
+    end
   end
 
 end
