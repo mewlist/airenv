013af792fb1df5625b79ebe023631700d76151ca
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2014-02-03 13:16:35 +0900

    Adds methods for installed SDKs

--- lib/airenv/sdk_description.rb
+++ lib/airenv/sdk_description.rb
@@ -31,4 +31,36 @@ class Airenv::SdkDescription
   def digest
     Digest::MD5.digest(id)
   end
+
+  class << self
+    def normalize_version_id(version_id)
+      if simple_version?(version_id)
+        installed_sdks.select {|sdk|
+          sdk.simple_version == version_id
+        }.first.description.id
+      else
+        version_id
+      end
+    end
+
+    def usable?(version_id)
+      installed_sdks.map {|sdk| [sdk.description.id, sdk.simple_version] }.flatten.include? version_id
+    end
+
+    def installed_sdks
+      installed_versions.map {|version| Airenv::Sdk.new(version) }
+    end
+
+    def simple_version?(version_id)
+      not version_id.include?("-b")
+    end
+
+    def installed_versions
+      Dir.glob("#{Settings.sdks_directory}/*").map {|f|
+        Pathname.new(f).basename.to_s
+      }.reject {|basename| basename == 'current' }.map {|basename|
+        basename.to_s.gsub("AIRSDK_", '')
+      }
+    end
+  end
 end
